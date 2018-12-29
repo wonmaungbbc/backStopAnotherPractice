6065bc2d69efd34f97ae39fa9b95cc6fe6567d9d
Won Maung Thein <wonmaungthein@gmail.com>
2018-12-29 17:35:54 +0000

    Deleted a line to check if it's working

--- backstop_data/html_report/config.js
+++ backstop_data/html_report/config.js
@@ -4,50 +4,54 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
-        "test": "../bitmaps_test/20181229-171224/backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
+        "test": "../bitmaps_test/20181229-173509/backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
         "selector": "document",
         "fileName": "backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
         "label": "BackstopJS Homepage",
         "requireSameDimensions": true,
         "misMatchThreshold": 0.1,
-        "url": "file:///Users/wonmaungthein/Desktop/myCoolProject/index.html",
+        "url": "file:///Users/wonmaungthein/Desktop/backStopAnotherPractice/index.html",
         "referenceUrl": "",
         "expect": 0,
         "viewportLabel": "phone",
         "diff": {
-          "isSameDimensions": true,
+          "isSameDimensions": false,
           "dimensionDifference": {
             "width": 0,
-            "height": 0
+            "height": -98
           },
-          "misMatchPercentage": "0.00"
-        }
+          "misMatchPercentage": "1.53",
+          "analysisTime": 28
+        },
+        "diffImage": "../bitmaps_test/20181229-173509/failed_diff_backstop_default_BackstopJS_Homepage_0_document_0_phone.png"
       },
-      "status": "pass"
+      "status": "fail"
     },
     {
       "pair": {
         "reference": "../bitmaps_reference/backstop_default_BackstopJS_Homepage_0_document_1_tablet.png",
-        "test": "../bitmaps_test/20181229-171224/backstop_default_BackstopJS_Homepage_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20181229-173509/backstop_default_BackstopJS_Homepage_0_document_1_tablet.png",
         "selector": "document",
         "fileName": "backstop_default_BackstopJS_Homepage_0_document_1_tablet.png",
         "label": "BackstopJS Homepage",
         "requireSameDimensions": true,
         "misMatchThreshold": 0.1,
-        "url": "file:///Users/wonmaungthein/Desktop/myCoolProject/index.html",
+        "url": "file:///Users/wonmaungthein/Desktop/backStopAnotherPractice/index.html",
         "referenceUrl": "",
         "expect": 0,
         "viewportLabel": "tablet",
         "diff": {
-          "isSameDimensions": true,
+          "isSameDimensions": false,
           "dimensionDifference": {
             "width": 0,
-            "height": 0
+            "height": -98
           },
-          "misMatchPercentage": "0.00"
-        }
+          "misMatchPercentage": "0.89",
+          "analysisTime": 47
+        },
+        "diffImage": "../bitmaps_test/20181229-173509/failed_diff_backstop_default_BackstopJS_Homepage_0_document_1_tablet.png"
       },
-      "status": "pass"
+      "status": "fail"
     }
   ],
   "id": "backstop_default"
